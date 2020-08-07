ed6afaa09c9bf702c861c4d26485203c3eb05016
Jan Sladký <sladky.honza@gmail.com>
2020-08-07 07:37:16 +0200

    prvni commit všeho

---
+++ assets/js/app.js
@@ -0,0 +1,42 @@
+$(".burger").on('click', function(){
+   
+    //vysunutí menu
+    $(".menu").toggleClass("show");
+  
+    // cross burger
+    $(".burger").toggleClass("toggle");
+  });
+
+                       
+  $(document).ready(function(){
+	  $(".content").hide().fadeIn(2000);
+
+	$('a[href^="#"]').on('click', function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var target = $(target);
+
+		$('html, body').animate({
+			'scrollTop': target.offset().top},
+			1000, 'swing', function () {
+				window.location.hash = target;
+
+			});
+
+	});
+
+	reklama
+	$('#main').addClass("reklama");
+
+
+	//responzivní menu
+	if ($(window).width() < 991) {
+		$('#navbarNavDropdown').addClass('collapse');
+
+	};
+
+
+
+
+});
