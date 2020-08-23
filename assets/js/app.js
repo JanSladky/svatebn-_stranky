123eee7e77c3adb0d710214f06aa61c8f9b0bf18
Jan Sladký <sladky.honza@gmail.com>
2020-08-23 17:53:18 +0200

    upravení js fce na kotvy a scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,33 @@
-//burger fce
-$(".burger").on('click', function(){
-   
-    //vysunutí menu
-    $(".menu").toggleClass("show");
-  
-    // cross burger
-    $(".burger").toggleClass("toggle");
-  });
+
+
 
   // hide content fce                     
   $(document).ready(function(){
 	  $(".content").hide().fadeIn(2000);
+	  
+	  //burger fce
+	  $(".burger").on('click', function(){
+   
+		//vysunutí menu
+		$(".menu").toggleClass("show");
+	  
+		// cross burger
+		$(".burger").toggleClass("toggle");
+	  });
 
 	//kotva fce 
-	$('a[href^="#"]').on('click', function (e) {
+		$('a[href^="#"]').on('click', function (e) {
 		e.preventDefault();
 
 		var target = this.hash;
 		var target = $(target);
 
 		$('html, body').animate({
-			'scrollTop': target.offset().top},
+			scrollTop: $(this.hash).offset().top - 75},
 			1000, 'swing', function () {
 				window.location.hash = target;
 
-			});
+		});
 
 	});
 
@@ -37,8 +40,4 @@ $(".burger").on('click', function(){
 		$('#navbarNavDropdown').addClass('collapse');
 
 	};
-
-
-
-
 });
